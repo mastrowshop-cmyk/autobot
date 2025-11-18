206ec60914c68717459c4c5c555842bcfa049a8b
mastrowshop-cmyk <mastrowshop@gmail.com>
2025-11-19 01:19:47 +0900

    Update __init__.py

--- bot/handlers/__init__.py
+++ bot/handlers/__init__.py
@@ -1,9 +1,10 @@
 from . import auth, manager, menu, client, admin
 
-all_routers = [
+all_routers = (
     auth.router,
     manager.router,
     admin.router,
     menu.router,
     client.router,
-]
+)
+
