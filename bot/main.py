337043de3892df192e573d119aa5330f0a2d8ed7
mastrowshop-cmyk <mastrowshop@gmail.com>
2025-11-19 01:16:27 +0900

    Update main.py

--- bot/main.py
+++ bot/main.py
@@ -2,21 +2,27 @@ import asyncio
 from aiogram import Bot, Dispatcher
 from aiogram.client.default import DefaultBotProperties
 
-from .config import load_config
-from .handlers import all_routers
-from .database import init_db
+from bot.config import load_config
+from bot.handlers import all_routers
+from bot.db import init_db
+
 
 async def main():
     cfg = load_config()
+
     await init_db()
+
     bot = Bot(
-        cfg.token,
+        token=cfg.token,
         default=DefaultBotProperties(parse_mode="HTML")
     )
+
     dp = Dispatcher()
     dp.include_router(all_routers)
+
     await bot.delete_webhook(drop_pending_updates=True)
     await dp.start_polling(bot)
+
 
 if __name__ == "__main__":
     asyncio.run(main())
